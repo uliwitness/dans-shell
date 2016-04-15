ea947abb35686926bf892533f342e5be1b96885f
Uli Kusterer <ulivc@zathras.de>
2016-04-15 22:25:52 +0200

    Add a main loop that requests input from stdin linewise and looks at it.

--- dansh/dansh/main.cpp
+++ dansh/dansh/main.cpp
@@ -7,9 +7,52 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+using namespace std;
+
+
+bool		gKeepRunning = true;
+
+
+void	process_one_line( const string & currLine )
+{
+	if( currLine.compare("exit") == 0 )
+		gKeepRunning = false;
+	else
+		cout << "Unknown command \"" << currLine.c_str() << "\"" << endl;
+}
+
+
+void	print_prompt()
+{
+	cout << "> ";
+}
+
+
+int main(int argc, const char * argv[])
+{
+	string		currLine;
+	
+	print_prompt();
+	
+	while( gKeepRunning )
+	{
+		char	currCh = 0;
+		currCh = getc(stdin);
+		if( currCh == EOF || currCh == '\0' )
+			gKeepRunning = false;
+		else if( currCh == '\n' || currCh == '\r' )
+		{
+			process_one_line( currLine );
+			currLine.erase();
+			
+			if( gKeepRunning )
+				print_prompt();
+		}
+		else
+			currLine.append( 1, currCh );
+	}
+	
     return 0;
 }
