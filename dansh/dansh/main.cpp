36e6e6f96a81353a35c34d08de4334bef1bd830e
Uli Kusterer <ulivc@zathras.de>
2016-04-15 22:24:44 +0200

    Add project for the shell proper.

---
+++ dansh/dansh/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dansh
+//
+//  Created by Uli Kusterer on 15/04/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
