001c8f62f80645d02487b48e19168d9fc5235b22
Uli Kusterer <ulivc@zathras.de>
2016-05-11 09:45:55 +0200

    Add support for -n option to our built-in echo command.

--- dansh/dansh/dansh_command_echo.cpp
+++ dansh/dansh/dansh_command_echo.cpp
@@ -15,10 +15,18 @@ using namespace std;
 
 dansh_built_in_lambda	dansh_command_echo = []( dansh_statement_ptr params )
 {
+	bool	addLineBreak = true;
     for( const dansh_statement_ptr currParam : params->params )
     {
+		if( currParam->name.compare("-n") == 0 )
+		{
+			addLineBreak = false;
+			continue;
+		}
+		
         cout << currParam->name;
     }
-    cout << endl;
+	if( addLineBreak )
+		cout << endl;
     return dansh_statement_ptr();
 };
